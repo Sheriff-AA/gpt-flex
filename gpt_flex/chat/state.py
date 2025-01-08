12eecb06101ad14ba4e00a366573e54892af3066
Sherif <sherifatitebi@gmail.com>
2025-01-08 03:10:53 +0000

    handling chat state

--- gpt_flex/chat/state.py
+++ gpt_flex/chat/state.py
@@ -1,7 +1,38 @@
+from typing import List
+import asyncio
 import reflex as rx
 
 
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
+
+
 class ChatState(rx.State):
-    def handle_submit(self, form_data: dict):
+    did_submit: bool = False
+    messages: List[ChatMessage] = []
+
+    @rx.var
+    def user_did_submit(self) -> bool:
+        return self.did_submit
+    
+    def append_message(self, message, is_bot: bool=False):
+        return self.messages.append(
+                ChatMessage(
+                    message=message,
+                    is_bot=is_bot,
+                )
+            )
+
+    async def handle_submit(self, form_data: dict):
         print(form_data)
+        user_message = form_data.get('message')
+        if user_message:
+            self.did_submit = True
+            self.append_message(user_message, False)
+            yield
+            await asyncio.sleep(2)
+            self.did_submit = False
+            self.append_message(user_message, True)
+            yield
 
